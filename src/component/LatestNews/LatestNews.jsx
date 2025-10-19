6d65a11cb7c488b1b78832efb1855c57d65f9e86
am-mahmud <am.mahmudasif@gmail.com>
2025-10-19 16:57:22 -0400

    Component added

---
+++ src/component/LatestNews/LatestNews.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LatestNews = () => {
+    return (
+        <div className='flex items-center gap-5 bg-base-300 p-3'>
+            <p className='text-base-100 bg-secondary p-3'>Latest</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, odio, quisquam ullam ratione expedita nihil maiores laboriosam sint, sapiente alias blanditiis dolorum praesentium earum neque? Neque recusandae voluptate nesciunt odio?</p>
+        </div>
+    );
+};
+
+export default LatestNews;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../component/Header/Header';
+import LatestNews from '../component/LatestNews/LatestNews';
 
 const HomeLayout = () => {
     return (
         <div>
             <Header></Header>
+            <section className='w-11/12 mx-auto mt-2'>
+                <LatestNews></LatestNews>
+            </section>
             <main>
                 <aside></aside>
                 <section>
